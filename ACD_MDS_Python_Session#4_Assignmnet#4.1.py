8a3618a583d90be2c3bb54f27b73364d5773b953
Sreenivasan V Rajangan <rvsreeni@gmail.com>
2017-12-26 10:20:29 +0530

    Add files via upload

---
+++ ACD_MDS_Python_Session#4_Assignmnet#4.1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+class Triangle:
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+        
+    def __str__(self):
+        return "Triangle has 3 sides %d, %d, %d ." % (self.side1, self.side2, self.side3)
+
+class Calculate(Triangle):
+    def __init__(self, *args, **kwargs):
+        super(Calculate, self).__init__(*args, **kwargs)
+        
+    def area(self):
+        s = (self.side1 + self.side2 + self.side3) / 2
+        return ((s*(s - self.side1)*(s - self.side2)*(s - self.side3)) ** 0.5)
+ 
+tri1 = Triangle(6, 8, 10)
+print(tri1)
+calc1 = Calculate(6, 8, 10)   
+print("Area of triangle is %d " % calc1.area())       
+        
+        
+        
+        
+        
+        
